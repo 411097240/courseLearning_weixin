996db2b1068c9b800f1813011816cc168b663caa
liangxiang <411097240@qq.com>
2018-01-23 14:41:52 +0800

    增加spring和jedis的整合

--- src/main/java/com/able/courseLearning_weixin/controller/validate/TestController.java
+++ src/main/java/com/able/courseLearning_weixin/controller/validate/TestController.java
@@ -3,10 +3,15 @@ package com.able.courseLearning_weixin.controller.validate;
 import com.able.courseLearning_weixin.common.pojo.AllUser;
 import com.able.courseLearning_weixin.dao.validate.IUserRegisterDao;
 import com.able.courseLearning_weixin.service.validate.impl.IUserRegisterService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.ShardedJedis;
+import redis.clients.jedis.ShardedJedisPool;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -18,9 +23,17 @@ public class TestController {
     IUserRegisterDao userRegisterDao;
     @Resource
     IUserRegisterService userRegisterService;
+    @Autowired
+    private ShardedJedisPool shardedJedisPool;//注入ShardedJedisPool
     @ResponseBody
     @RequestMapping(value = "test",method = RequestMethod.GET)
     public Object test(HttpServletRequest request, HttpServletResponse response){
-        return userRegisterService.doRegister(new AllUser(),request,response);
+       // return userRegisterService.doRegister(new AllUser(),request,response);
+        ShardedJedis shardedJedis = shardedJedisPool.getResource();
+        //根据键值获得数据
+        String result = shardedJedis.get("key2");
+        shardedJedis.close();
+
+        return result;
     }
 }
